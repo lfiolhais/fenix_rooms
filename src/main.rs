866d0d91c5e6a9eec92f712d7e258d5c8e0bdcb5
Luís Fiolhais <luisazenhas.fiolhais@openmailbox.org>
2016-12-04 18:01:26 +0000

    Get all campi

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,26 @@ use std::collections::BTreeMap;
 use pencil::{Pencil, Request, Response, PencilResult};
 use pencil::jsonify;
 use hyper::client::Client;
+use std::io::Read;
 
-//! Creates a User in the Database
-//!
-//! Create a user in the database with the specified username in the body. If
-//! multiple usernames are provided only the first will be considered. If the no
-//! username is provided an error message will be displayed with the status code
-//! 204. Otherwise the username will be created and the JSON message will be
-//! sent with the username and the ID given by the DB.
-//!
-//! # Arguments
-//! * request - The request sent by the client
-//!
-//! # Output
-//! A Response with a JSON messsage and correct status code.
+/// Creates a User in the Database
+///
+/// Create a user in the database with the specified username in the body. If
+/// multiple usernames are provided only the first will be considered. If the no
+/// username is provided an error message will be displayed with the status code
+/// 204. Otherwise the username will be created and the JSON message will be
+/// sent with the username and the ID given by the DB.
+///
+/// # Arguments
+/// * request - The request sent by the client
+///
+/// # Output
+/// A Response with a JSON messsage and correct status code.
 fn create_user(request: &mut Request) -> PencilResult {
 
     // Get the username from the body of the request if it exists
     let username: &str = match request.form().get("Some") {
-        username(username) => username,
+        Some(username) => username,
         None => "",
     };
 
@@ -51,29 +52,51 @@ fn create_user(request: &mut Request) -> PencilResult {
     return jsonify(&object);
 }
 
+/// Get all campi at IST
+///
+/// # Output
+/// All campi
+fn get_campi(request: &mut Request) -> PencilResult {
+    // Create Hyper client to perform REST calls
+    let client = Client::new();
+
+    // Create and send GET request
+    let mut res =
+        client.get("https://fenix.tecnico.ulisboa.pt/api/fenix/v1/spaces").send().unwrap();
+    // Read content from response and write it to a buffer
+    let mut buf: String = String::new();
+    res.read_to_string(&mut buf);
+
+    // Build response and set contetn to JSON
+    let mut response = Response::from(buf);
+    response.set_content_type("application/json");
+
+    return Ok(response);
+}
+
 fn main() {
     let mut app = Pencil::new("~/fenix-rooms/src");
     app.set_debug(true);
     app.set_log_level();
     env_logger::init().unwrap();
 
-    /// ///////////////////////////////////////////////////////
-    /// Routing
-    /// ///////////////////////////////////////////////////////
+    // ///////////////////////////////////////////////////////
+    // Routing
+    // ///////////////////////////////////////////////////////
 
-    /// ///////////////////////////////////////////////////////
-    /// Templates
-    /// The standard browser routes through here.
-    /// ///////////////////////////////////////////////////////
+    // //////////////////////////////////////////////////////
+    // Templates
+    // The standard browser routes through here.
+    // ///////////////////////////////////////////////////////
 
-    /// ///////////////////////////////////////////////////////
-    /// REST API
-    /// The REST API will only return JSON enconded responses.
-    /// ///////////////////////////////////////////////////////
+    // ///////////////////////////////////////////////////////
+    // REST API
+    // The REST API will only return JSON enconded responses.
+    // ///////////////////////////////////////////////////////
 
     // Admin
     // Admin always has user-id 0
-    app.get("/api/user-id/0", "admin", get_all_available);
+    app.get("/api/user-id/0/campi", "admin", get_campi);
 
     // User
     // User can have any other user-id except 0
