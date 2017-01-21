af6bd8e86f019497667d6bbe23a992e748ad04b8
Luís Fiolhais <luisazenhas.fiolhais@openmailbox.org>
2017-01-21 18:22:28 +0000

    Create or read default config
    Finalize the CLI

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,7 @@
+/// Client CLI for the Fenix Rooms web service
+/// Author: Luís Fiolhais
+///
+///
 extern crate fenix_rooms;
 #[macro_use]
 extern crate clap;
@@ -8,9 +12,40 @@ use clap::App;
 use fenix_rooms::utils::{delete_request, get_request, post_request, read_response_body};
 use fenix_rooms::api::GenericSpace;
 
-const SERVER_URL: &'static = "https://fenix-rooms.herokuapp.com";
+use std::fs::File;
+use std::io::Write;
+
+const DEFAULT_CONFIG_TOML: &'static str = r#"
+    [session]
+    server_url = "https://fenix-rooms.herokuapp.com"
+    user_id = ""
+    username = ""
+"#;
 
 fn main() {
+    // Build CLI parser
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+
+    // Try to read config file
+    let mut file: File = match File::open("fenix-rooms-user.toml") {
+        Ok(file) => file,
+        // Assume that if the open file fails the file doesn't exist.
+        // Create file and write defaults
+        Err(_) => {
+            let mut new_file: File = match File::create("fenix-rooms-user.toml") {
+                Ok(new_file) => new_file,
+                Err(err) => panic!("Failed to create file fenix-rooms-user.toml with: {}", err),
+            };
+            match new_file.write_all(DEFAULT_CONFIG_TOML.as_bytes()) {
+                Ok(_) => (),
+                Err(err) => {
+                    panic!("Failed to write the contents of the default file with: {}",
+                           err)
+                }
+            };
+            new_file
+        }
+    };
+
 }
